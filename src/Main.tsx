d71c7815c6015da1bb29bc03bc7e120cac90e1ca
Jonathan Powell <jpowell.cr@gmail.com>
2021-12-14 09:16:04 -0600

    chore: remove Detail component

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Detail from './pages/Detail';
 
 const Main = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
-      <Route path='/detail' element={<Detail />}></Route>
     </Routes>
   );
 };
